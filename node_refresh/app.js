2302feb02d3156284e261bd35efc864191e22e47
alouzao10 <alouzao10@gmail.com>
2021-03-04 17:32:51 -0500

    refresher on built in node modules

--- node_refresh/app.js
+++ node_refresh/app.js
@@ -23,5 +23,85 @@
 */
 
 // Load in an external module
-const logger = require('./logger.js');
+const Logger = require('./logger.js');
+const logger = new Logger();
+logger.on('messageLogged', (e) => {
+  console.log('Event Message Has Been Logged...');
+  console.log('Event Argument...', e);
+});
 logger.logMessage('Hello Module...');
+
+// Load in the path module
+const path = require('path');
+var pathFile = path.parse(__filename);
+console.log(pathFile);
+var pathDir = path.parse(__dirname);
+console.log(pathDir);
+
+// Load in the os module
+const os = require('os');
+var totalMem = os.totalmem();
+console.log(`Total Memory = ${totalMem}`);
+var freeMem = os.freemem();
+console.log(`Free Memory = ${freeMem}`);
+
+// Load in the fs module
+const fs = require('fs');
+// Sync Method
+var files = fs.readdirSync('./');
+console.log(files);
+// Async Method
+/*fs.readdir('./', function (err, files) {
+  if (err) {
+    console.log('fs read ERROR...');
+  } else {
+    console.log('fs read SUCCESS...', files);
+  }
+});
+
+fs.readdir('./something', function (err, files) {
+  if (err) {
+    console.log('fs read ERROR...', err);
+  } else {
+    console.log('fs read SUCCESS...', files);
+  }
+});*/
+
+// Load in the events module
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+// Register an event listener to capture the event
+// The listener can receive the argument (arg / e)
+emitter.on('messageLogged', (e) => {
+  console.log('Event Message Has Been Logged...');
+  console.log('Event Argument...', e);
+});
+// Raise an event / event will be called
+// Pass in an argument to better id the event
+emitter.emit('messageLogged', { id: 1, url: 'https://' });
+
+// Load in the http module
+const http = require('http');
+// Creates a web server, acts as an EventEmitter
+// Can capture the server requests and provide responses as a result
+const server = http.createServer((request, response) => {
+  if (request.url === '/') {
+    response.write('Hello Home');
+    response.end();
+  } else if (request.url === '/about') {
+    response.write('Hello About');
+    response.end();
+  }
+  // Can capture data from an API and return into the code for use
+  if (request.url === '/api/courses') {
+    response.write(JSON.stringify([1, 2, 3]));
+    response.end();
+  }
+});
+const PORT_NUM = 3000;
+// Creates an even handler based on the event, such as connecting to the server
+server.on('connection', (socket) => {
+  console.log(`Connection Made on Server: ${PORT_NUM}`);
+});
+// Have the server listen to a specific server port number
+server.listen(PORT_NUM);
--- node_refresh/logger.js
+++ node_refresh/logger.js
@@ -1,16 +1,31 @@
 // External Module for app.js
 let phrase = 'Cool Story Bro!';
 
-let logMessage = (msg) => {
+// Load in the events module
+const EventEmitter = require('events');
+//const emitter = new EventEmitter();
+
+// Create a class to be exported for the emitter to have access
+class Logger extends EventEmitter {
+  logMessage = (msg) => {
+    // Send a request
+    console.log(`In the Logger Module...${phrase}`);
+    console.log(msg);
+    this.emit('messageLogged', { id: 5, url: 'Logger Event' });
+  };
+}
+/*let logMessage = (msg) => {
   // Send a request
   console.log(`In the Logger Module...${phrase}`);
   console.log(msg);
-};
+  emitter.emit('messageLogged', { id: 5, url: msg });
+};*/
 
 // Use to export the module to the main app file
 // Name can be renamed on the assignment
-module.exports.logMessage = logMessage;
-module.exports.message = phrase;
+//module.exports.logMessage = logMessage;
+//module.exports.message = phrase;
+module.exports = Logger;
 
 // Export a single item like a function
 // module.exports = logMessage;
